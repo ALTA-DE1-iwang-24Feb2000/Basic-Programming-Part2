d06cfcbc333f50e996760fb89149d097c221d536
moeslemiwang <moeslemiwang@gmail.com>
2023-09-27 21:52:01 +0700

    upload tugas

--- problem3/main.py
+++ problem3/main.py
@@ -1,6 +1,9 @@
 # input
 T = 20.0
 r = 4.0
-
+pi = 3.14
 
 # kode disini
+
+lp = 2*pi*(r**2) + 2 * pi * r * T 
+print(lp)
--- problem4/main.py
+++ problem4/main.py
@@ -3,3 +3,11 @@ Input Harga: 370000
 Input Diskon: 15
 Output: harga yang harus dibayar adalah Rp. 314.500
 '''
+
+harga_awal = int(input("masukan harga awal: " ))
+diskon = int(input("masukkan diskon: " ))
+
+harga_diskon = (diskon/100) * harga_awal
+harga_akhir = harga_awal - harga_diskon
+
+print (harga_akhir)
